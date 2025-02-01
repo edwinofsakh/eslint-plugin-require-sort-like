684131082fef28baad6729266cfc1008d7574c4e
edwinofsakh <edwinofsakh@gmail.com>
2025-02-01 12:59:40 +0200

    fix: improve declarations autofix

--- tests/lib/rules/sort-declarations.js
+++ tests/lib/rules/sort-declarations.js
@@ -16,85 +16,86 @@ const rule = require("../../../lib/rules/sort-declarations"),
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
-ruleTester.run("sort", rule, {
+ruleTester.run("sort-declarations", rule, {
   valid: [
     {
       code: `
-const a = require('aaa');
-const b = require('bbb');
-`,
+    const a = require('aaa');
+    const b = require('bbb');
+    `,
     },
     {
       code: `
-const b = require('aaa');
-const a = require('bbb');
-`,
+    const b = require('aaa');
+    const a = require('bbb');
+    `,
     },
     {
       code: `
-const a = require('aaa');
-require('bbb');
-`,
+    const a = require('aaa');
+    require('bbb');
+    `,
     },
     {
       code: `
-const c = require('bbb');
-const b = require('./aaa');
-const a = require('../aaa');
-`,
+    const c = require('bbb');
+    const b = require('./aaa');
+    const a = require('../aaa');
+    `,
     },
     {
       code: `
-const f = require('@foo');
-const a = require('bar');
-const b = require('foo');
-const c = require('./foo');
-const { d } = require('../foo');
-const e = require('../../foo');
-`,
+    const f = require('@foo');
+    const a = require('bar');
+    const b = require('foo');
+    const c = require('./foo');
+    const { d } = require('../foo');
+    const e = require('../../foo');
+    `,
     },
   ],
 
   invalid: [
     {
       code: `
-const a = require('bar');
-const b = require('foo');
-const c = require('./foo1');
-const d = require('../foo2');
-const e = require('../../foo3');
-const f = require('@foo');
+const b = require('a2');
+const a = require('a1');
 `,
       output: `
-const f = require('@foo');
-const a = require('bar');
-const b = require('foo');
-const c = require('./foo1');
-const d = require('../foo2');
-const e = require('../../foo3');
+const a = require('a1');
+const b = require('a2');
 `,
-      errors: [
-        { messageId: "MSG", type: "VariableDeclaration" },
-        { messageId: "MSG", type: "VariableDeclaration" },
-        { messageId: "MSG", type: "VariableDeclaration" },
-        { messageId: "MSG", type: "VariableDeclaration" },
-        { messageId: "MSG", type: "VariableDeclaration" },
-        { messageId: "MSG", type: "VariableDeclaration" },
-      ],
+      errors: [{ messageId: "MSG", type: "VariableDeclaration" }],
     },
     {
       code: `
-require('bbb');
-const a = require('aaa');
-`,
+    const a = require('a2');
+    const b = require('a3');
+    const c = require('./a4');
+    const d = require('../a5');
+    const e = require('../../a6');
+    const f = require('@a1');
+    `,
       output: `
-const a = require('aaa');
-require('bbb');
-`,
-      errors: [
-        { messageId: "MSG", type: "ExpressionStatement" },
-        { messageId: "MSG", type: "VariableDeclaration" },
-      ],
+    const f = require('@a1');
+    const a = require('a2');
+    const b = require('a3');
+    const c = require('./a4');
+    const d = require('../a5');
+    const e = require('../../a6');
+    `,
+      errors: [{ messageId: "MSG", type: "VariableDeclaration" }],
+    },
+    {
+      code: `
+    require('a2');
+    const a = require('a1');
+    `,
+      output: `
+    const a = require('a1');
+    require('a2');
+    `,
+      errors: [{ messageId: "MSG", type: "ExpressionStatement" }],
     },
   ],
 });
